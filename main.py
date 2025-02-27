dd16ef2c1892d193cab17f7038456a9ba26ac43d
ZdenekSrotyr <zdenek.srotyr@keboola.com>
2025-02-27 17:25:31 +0100

    Reorganize

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 import os
 import sys
+
+# Add the current directory to the Python path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+if current_dir not in sys.path:
+    sys.path.insert(0, current_dir)
+    
 import argparse
 
 from src.config import GITHUB_TOKEN, logger, RELEASE_NOTES_DIR

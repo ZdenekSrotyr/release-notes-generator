24e7abfc4047065af7204248a415562e1f3670a0
ZdenekSrotyr <zdenek.srotyr@keboola.com>
2025-02-27 17:27:50 +0100

    Reorganize

--- main.py
+++ main.py
@@ -2,10 +2,9 @@
 import os
 import sys
 
-# Add the current directory to the Python path
+# Add the project root directory to the Python path
 current_dir = os.path.dirname(os.path.abspath(__file__))
-if current_dir not in sys.path:
-    sys.path.insert(0, current_dir)
+sys.path.insert(0, current_dir)
     
 import argparse
 
